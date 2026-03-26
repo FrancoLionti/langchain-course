643a3bc8bbe8962eb61cbf899a15e279d71d5aa8
Fran <franluntref@gmail.com>
2026-03-26 11:36:43 -0300

    proceed with next module implemented two step RAG pipeline for augmented context response

--- rag-gist/ingestion.py
+++ rag-gist/ingestion.py
@@ -1,9 +1,23 @@
 import os
 from dotenv import load_dotenv
-
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting...")
+    loader = TextLoader("mediumblog1.txt", encoding="utf-8")
+    documents = loader.load()
+    print(f"Loaded {len(documents)} documents")
+    print("Splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split into {len(chunks)} chunks")
 
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    print("Ingesting...")
+    PineconeVectorStore.from_documents(chunks, embeddings, index_name=os.environ["PINECONE_INDEX_NAME"])
+    print("Done!")
